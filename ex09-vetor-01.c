18fa8df7086c66f0483a3077198384fcfcf68cb1
cpmart <cpmart@gmail.com>
2023-09-21 10:43:32 -0300

    vetor1

---
+++ ex09-vetor-01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h> //acessar a função srand()
+#include <time.h>   //pegar o time da máquina
+#define N 5
+int main(){
+    int vet[N];
+
+    vet[0] = 3;
+    vet[1] = 31;
+    vet[2] = 35;
+    vet[3] = 38;
+    vet[4] = 39;
+    
+    scanf("%d",&vet[0]);
+    scanf("%d",&vet[1]);
+    scanf("%d",&vet[2]);
+    scanf("%d",&vet[3]);
+    scanf("%d",&vet[4]);
+
+    
+}
