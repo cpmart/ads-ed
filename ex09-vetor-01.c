66b5c55966ee4d4cd460cf2e143000cba7337362
cpmart <cpmart@gmail.com>
2023-09-21 10:57:24 -0300

    vetor2

--- ex09-vetor-01.c
+++ ex09-vetor-01.c
@@ -3,9 +3,9 @@
 #include <time.h>   //pegar o time da máquina
 #define N 5
 int main(){
-    int vet[N];
+    int vet[N], i=25;
 
-    vet[0] = 3;
+   vet[0] = 3;
     vet[1] = 31;
     vet[2] = 35;
     vet[3] = 38;
@@ -15,7 +15,11 @@ int main(){
     scanf("%d",&vet[1]);
     scanf("%d",&vet[2]);
     scanf("%d",&vet[3]);
-    scanf("%d",&vet[4]);
+    scanf("%d",&vet[4]); 
 
-    
+    srand(time(NULL));
+    for(int i=0; i<N; i++){
+        vet[i]=rand()%6;
+        printf("%d\n",vet[i]);
+    }
 }
